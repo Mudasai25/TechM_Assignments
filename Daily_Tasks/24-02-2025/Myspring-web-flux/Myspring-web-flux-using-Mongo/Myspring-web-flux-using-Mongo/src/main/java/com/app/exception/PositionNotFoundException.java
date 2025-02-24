f3ad4901db8c43738bb6b42b3f3459ef4d8f8533
MUDA SAI VENKATA RAMANA <138794814+Mudasai25@users.noreply.github.com>
2025-02-24 22:58:59 +0530

    Delete Daily_Tasks/24-02-2025/Myspring-web-flux directory

--- Daily_Tasks/24-02-2025/Myspring-web-flux/Myspring-web-flux-using-Mongo/Myspring-web-flux-using-Mongo/src/main/java/com/app/exception/PositionNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.app.exception;
-
-public class PositionNotFoundException extends RuntimeException {
-
-    public PositionNotFoundException(String positionId) {
-        super("Position not found with id " + positionId);
-  }
-}
--- Daily_Tasks/24-02-2025/Myspring-web-flux/Myspring-web-flux/src/main/java/com/app/exception/TweetNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.app.exception;
-public class TweetNotFoundException extends RuntimeException {
-
-    public TweetNotFoundException(String tweetId) {
-        super("Tweet not found with id " + tweetId);
-    }
-}
